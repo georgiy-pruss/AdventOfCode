ee511777715cf6493215a030dc4a56cc70c0ea44
Georgiy Pruss <yru2l8@yahoo.com>
2015-12-23 10:25:16 +0200

    Update 22-part2.c

--- 22-part2.c
+++ 22-part2.c
@@ -8,7 +8,7 @@
 
 // State: mana hits armor  b-hits b-damage  shield poison recharge  total-spent
 typedef struct St { int m,h,a, b,d, sh,po,re, t; } State;
-enum {O,X,M,D,S,P,R,B}; // spell coding; X-empty, B-it's boss' turn, O-like '\0', eos
+enum {X,M,D,S,P,R,B}; // spell coding; X: empty, B: boss's turn
 
 int turn( State* s, int w ) __ // w: which spell to choose
   int a=s->a;
@@ -29,10 +29,10 @@ int turn( State* s, int w ) __ // w: which spell to choose
     if( s->h<=0 ) return -1; _ // Boss won
   return 0; _
 
-int run( int* ww ) __
+int run( int* ww, int k ) __
   State s = {500,50,0,55,8,0,0,0,0};
   int r;
-  for( int w=*ww++; w; w=*ww++ ) __
+  for( int w=*ww++; k-->0; w=*ww++ ) __
     if( (r=turn( &s, w ))!=0 ) return r;
     if( (r=turn( &s, B ))!=0 ) return r; _
   return 0; _
@@ -40,29 +40,12 @@ int run( int* ww ) __
 int main() __
   int v[11] = {0}; int min_v[11]={0};
   int t; int min_t=9999;
-  for( int c1=X; c1<=R; ++c1 ) __
-    v[0]=c1;
-    for( int c2=X; c2<=R; ++c2 ) __
-      v[1]=c2;
-      for( int c3=X; c3<=R; ++c3 ) __
-        v[2]=c3; printf( "%d%d%d....... %d\r", c1,c2,c3, min_t ); fflush(stdout);
-        for( int c4=X; c4<=R; ++c4 ) __
-          v[3]=c4;
-          for( int c5=X; c5<=R; ++c5 ) __
-            v[4]=c5;
-            for( int c6=X; c6<=R; ++c6 ) __
-              v[5]=c6;
-              for( int c7=X; c7<=R; ++c7 ) __
-                v[6]=c7;
-                for( int c8=X; c8<=R; ++c8 ) __
-                  v[7]=c8;
-                  for( int c9=X; c9<=R; ++c9 ) __
-                    v[8]=c9;
-                    for( int cA=X; cA<=R; ++cA ) __
-                      v[9]=cA;
-                      t = run( v );
-                      if( t>0 && t<min_t ) { min_t=t; memcpy(min_v,v,sizeof(v)); }
-  _ _ _ _ _ _ _ _ _ _
+  #define DO(z) for(z=X;z<=R;++z)__
+  DO(v[0]) DO(v[1]) DO(v[2])
+    printf( "%d%d%d....... %d\r", v[0],v[1],v[2], min_t ); fflush(stdout);
+    DO(v[3]) DO(v[4]) DO(v[5]) DO(v[6]) DO(v[7]) DO(v[8]) DO(v[9])
+      t = run( v, 10 );
+      if( t>0 && t<min_t ) { min_t=t; memcpy(min_v,v,sizeof(v)); } _ _ _ _ _ _ _ _ _ _
   long long m=0; for(int i=0;i<10;++i) m=m*10+min_v[i]; // convert to a number for print
-  printf( "\n%lld %d\n", m, min_t ); // 5113125111 1289
+  printf( "\n%lld %d\n", m, min_t ); // 4002014000 1289
   return 0; _
