db73ab6ff7416096e055dd633624a3f7cb919972
Georgiy Pruss <yru2l8@yahoo.com>
2015-12-23 23:49:50 +0200

    Removed 'zero' variants in 'turn' fn too

--- 22b.c
+++ 22b.c
@@ -17,12 +17,13 @@ int turn( State* s, int w ) __ // w: which spell to choose
   if( s->re>0 ) { s->m+=101; s->re--; }
   if( s->po>0 ) { s->b-=3; s->po--; }
   if( s->b<=0 ) return s->t; // Player won
+  #define ERB else return 0; break
   switch(w) __
-    case M: if( s->m>=53 ) { s->b-=4;              s->m-= 53; s->t+= 53; } break;
-    case D: if( s->m>=73 ) { s->b-=2; s->h+=2;     s->m-= 73; s->t+= 73; } break;
-    case S: if( s->sh==0 && s->m>=113 ) { s->sh=6; s->m-=113; s->t+=113; } break;
-    case P: if( s->po==0 && s->m>=173 ) { s->po=6; s->m-=173; s->t+=173; } break;
-    case R: if( s->re==0 && s->m>=229 ) { s->re=5; s->m-=229; s->t+=229; } break; _
+    case M: if( s->m>=53 ) { s->b-=4;              s->m-= 53; s->t+= 53; } ERB;
+    case D: if( s->m>=73 ) { s->b-=2; s->h+=2;     s->m-= 73; s->t+= 73; } ERB;
+    case S: if( s->sh==0 && s->m>=113 ) { s->sh=6; s->m-=113; s->t+=113; } ERB;
+    case P: if( s->po==0 && s->m>=173 ) { s->po=6; s->m-=173; s->t+=173; } ERB;
+    case R: if( s->re==0 && s->m>=229 ) { s->re=5; s->m-=229; s->t+=229; } ERB; _
   if( s->b<=0 ) return s->t; // Player won
   if( w==B ) __
     s->h=s->h-(s->d-a<1?1:s->d-a);
