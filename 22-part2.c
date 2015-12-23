8abc304726c5d0569bb8a8f1513b66c2c1301177
Georgiy Pruss <yru2l8@yahoo.com>
2015-12-23 19:43:10 +0200

    Update 22-part2.c

--- 22-part2.c
+++ 22-part2.c
@@ -18,11 +18,11 @@ int turn( State* s, int w ) __ // w: which spell to choose
   if( s->po>0 ) { s->b-=3; s->po--; }
   if( s->b<=0 ) return s->t; // Player won
   switch(w) __
-    case M: if( s->m>=53 ) { s->b-=4;              s->m-= 53; s->t+= 53; }
-    case D: if( s->m>=73 ) { s->b-=2; s->h+=2;     s->m-= 73; s->t+= 73; }
-    case S: if( s->sh==0 && s->m>=113 ) { s->sh=6; s->m-=113; s->t+=113; }
-    case P: if( s->po==0 && s->m>=173 ) { s->po=6; s->m-=173; s->t+=173; }
-    case R: if( s->re==0 && s->m>=229 ) { s->re=5; s->m-=229; s->t+=229; } _
+    case M: if( s->m>=53 ) { s->b-=4;              s->m-= 53; s->t+= 53; } break;
+    case D: if( s->m>=73 ) { s->b-=2; s->h+=2;     s->m-= 73; s->t+= 73; } break;
+    case S: if( s->sh==0 && s->m>=113 ) { s->sh=6; s->m-=113; s->t+=113; } break;
+    case P: if( s->po==0 && s->m>=173 ) { s->po=6; s->m-=173; s->t+=173; } break;
+    case R: if( s->re==0 && s->m>=229 ) { s->re=5; s->m-=229; s->t+=229; } break; _
   if( s->b<=0 ) return s->t; // Player won
   if( w==B ) __
     s->h=s->h-(s->d-a<1?1:s->d-a);
@@ -31,8 +31,7 @@ int turn( State* s, int w ) __ // w: which spell to choose
 
 int run( int* ww, int k ) __
   State s = {500,50,0,55,8,0,0,0,0};
-  int r;
-  for( int w=*ww++; k-->0; w=*ww++ ) __
+  for( int r,w=*ww++; k-->0; w=*ww++ ) __
     if( (r=turn( &s, w ))!=0 ) return r;
     if( (r=turn( &s, B ))!=0 ) return r; _
   return 0; _
