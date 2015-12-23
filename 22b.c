77d62ece51787677719541fd2b6af7a424f51aa4
Georgiy Pruss <yru2l8@yahoo.com>
2015-12-24 00:40:20 +0200

    more optimizations

--- 22b.c
+++ 22b.c
@@ -1,23 +1,22 @@
 // wizard rpg part2 http://adventofcode.com/day/22 *** gcc -std=c99 -O2 22b.c && a.exe
 
 #include <stdio.h>
-#include <string.h>
 
 #define __ {
 #define _  }
 
 // State: mana hits armor  b-hits b-damage  shield poison recharge  total-spent
 typedef struct St { int m,h,a, b,d, sh,po,re, t; } State;
-enum {X,M,D,S,P,R,B}; // spell coding; X: empty, B: boss's turn
+enum {Z,M,D,S,P,R,B}; // spell coding; Z: empty, B: boss's turn
 
 int turn( State* s, int w ) __ // w: which spell to choose
   int a=s->a;
-  if( w!=B && (--s->h)<=0 ) return -1; // hard mode
+  if( w!=B && (--s->h)<=0 ) return -1; // part 2 = hard mode
   if( s->sh>0 ) { s->sh--; if( s->sh>0 ) a+=7; }
-  if( s->re>0 ) { s->m+=101; s->re--; }
   if( s->po>0 ) { s->b-=3; s->po--; }
+  if( s->re>0 ) { s->m+=101; s->re--; }
   if( s->b<=0 ) return s->t; // Player won
-  #define ERB else return 0; break
+  #define ERB else return -1; break // ~ Lost because cannot buy what planned
   switch(w) __
     case M: if( s->m>=53 ) { s->b-=4;              s->m-= 53; s->t+= 53; } ERB;
     case D: if( s->m>=73 ) { s->b-=2; s->h+=2;     s->m-= 73; s->t+= 73; } ERB;
@@ -31,21 +30,19 @@ int turn( State* s, int w ) __ // w: which spell to choose
   return 0; _
 
 int run( int* ww, int k ) __
-  State s = {500,50,0,55,8,0,0,0,0};
+  State s = {500,50,0, 55,8, 0,0,0, 0};
   for( int r,w=*ww++; k-->0; w=*ww++ ) __
-    if( (r=turn( &s, w ))!=0 ) return r;
-    if( (r=turn( &s, B ))!=0 ) return r; _
+    if( (r = turn( &s, w )) != 0 ) return r;
+    if( (r = turn( &s, B )) != 0 ) return r; _
   return 0; _
 
 int main() __
-  int v[10] = {0}; int min_v[10]={0};
-  int t; int min_t=9999;
+  int v[10] = {0}; int min_v[10]={0}; int n; int min_n=9999;
   #define DO(z) for(z=M;z<=R;++z)__
-  DO(v[0]) DO(v[1]) DO(v[2])
-    printf( "%d%d%d....... %d\r", v[0],v[1],v[2], min_t ); fflush(stdout);
-    DO(v[3]) DO(v[4]) DO(v[5]) DO(v[6]) DO(v[7]) DO(v[8]) DO(v[9])
-      t = run( v, 10 );
-      if( t>0 && t<min_t ) { min_t=t; memcpy(min_v,v,sizeof(v)); } _ _ _ _ _ _ _ _ _ _
-  long long m=0; for(int i=0;i<10;++i) m=m*10+min_v[i]; // convert to a number for print
-  printf( "%lld %d\n", m, min_t ); // 4002014000 1289 ... 1256 ... 4254354200 1236
+  DO(v[0]) DO(v[1]) DO(v[2]) DO(v[3]) DO(v[4]) DO(v[5]) DO(v[6]) DO(v[7]) DO(v[8])
+    n = run( v, 9 );
+    if( n>0 && n<min_n )
+      { min_n=n; for(int i=0;i<9;++i) min_v[i]=v[i]; } _ _ _ _ _ _ _ _ _
+  long long m=0; for(int i=0;i<9;++i) m=m*10+min_v[i]; // convert to a number for print
+  printf( "%lld %d\n", m, min_n ); // 425435421 1289 = PDRPSRPDM
   return 0; _
